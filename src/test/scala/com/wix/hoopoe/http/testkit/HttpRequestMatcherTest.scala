fe292341b10ef9e6f0086906a56e1136315ee8e0
Netta Doron <nettad@wix.com>
2017-02-21 15:11:21 +0200

    fixed HttpRequestMatcher test

--- src/test/scala/com/wix/hoopoe/http/testkit/HttpRequestMatcherTest.scala
+++ src/test/scala/com/wix/hoopoe/http/testkit/HttpRequestMatcherTest.scala
@@ -10,52 +10,51 @@ class HttpRequestMatcherTest extends SpecificationWithJUnit {
 
   "HttpRequestMatcher" should {
     "match on HttpRequest method" in new ctx {
-      val request = anHttpRequest.copy(method = HttpMethods.GET)
-      HttpRequestMatcher(method = Some(HttpMethods.POST)).matches(request) must beFalse
+      val request = aPostHttpRequest.copy(method = HttpMethods.GET)
+      aPostHttpRequestMatcher.matches(request) must beFalse
     }
 
     "match on HttpRequest uri" in new ctx {
-      val request = anHttpRequest.copy(uri = Uri.Path("/my-path").toString)
-      HttpRequestMatcher(uri = Some(Uri.Path("/some-other-path").toString)).matches(request) must beFalse
+      val request = aGetHttpRequest.copy(uri = Uri.Path("/my-path").toString)
+      aGetHttpRequestMatcher.copy(uri = Some(Uri.Path("/some-other-path").toString)).matches(request) must beFalse
     }
 
     "match on HttpRequest one header" in new ctx {
-      val request = anHttpRequest.copy(headers = List(Host("localhost", 1010)))
-      HttpRequestMatcher(headers = Some(Seq(Host("192.1.1.1", 1010)))).matches(request) must beFalse
+      val request = aGetHttpRequest.copy(headers = List(Host("localhost", 1010)))
+      aGetHttpRequestMatcher.copy(headers = Some(Seq(Host("192.1.1.1", 1010)))).matches(request) must beFalse
     }
 
     "matches on HttpRequest on more than one header" in new ctx {
       val host = Host("localhost", 1010)
-      val request = anHttpRequest.copy(headers = List(host, Location(Uri.Path("far/far/away").toString())))
-      HttpRequestMatcher(headers = Some(Seq(host))).matches(request) must beTrue
+      val request = aGetHttpRequest.copy(headers = List(host, Location(Uri.Path("far/far/away").toString())))
+      aGetHttpRequestMatcher.copy(headers = Some(Seq(host))).matches(request) must beTrue
     }
 
     "match on HttpRequest httpEntity" in new ctx {
-      val request = anHttpRequest.copy(entity = HttpEntity("some entity"))
-      HttpRequestMatcher(entity = Some(HttpEntity("another entity"))).matches(request) must beFalse
+      val request = aPostHttpRequest.copy(entity = HttpEntity("some entity"))
+      aGetHttpRequestMatcher.copy(entity = Some(HttpEntity("another entity"))).matches(request) must beFalse
     }
 
     "match on HttpRequest protocol" in new ctx {
-      val request = anHttpRequest.copy(protocol = HttpProtocols.`HTTP/1.1`)
-      HttpRequestMatcher(protocol = Some(HttpProtocols.`HTTP/1.0`)).matches(request) must beFalse
+      val request = aPostHttpRequest.copy(protocol = HttpProtocols.`HTTP/1.1`)
+      aGetHttpRequestMatcher.copy(protocol = Some(HttpProtocols.`HTTP/1.0`)).matches(request) must beFalse
     }
 
-    "match with more than one matcher" in new ctx {
-      val method = HttpMethods.GET
-      val path = Uri.Path("/my-path")
-      val request = anHttpRequest.copy(method = method, uri = path.toString)
-      HttpRequestMatcher(method = Some(method), uri = Some(path.toString)).matches(request) must beTrue
-    }
-
-    "match any HttpRequest" in new ctx {
-      val request = anHttpRequest
-      HttpRequestMatcher().matches(request) must beTrue
+    "match any HttpRequest by method only" in new ctx {
+      val request = aGetHttpRequest
+      aGetHttpRequestMatcher.matches(request) must beTrue
     }
   }
 
   trait ctx extends Scope {
-    val anHttpRequest =
+    def aPostHttpRequest =
       HttpRequest(method = HttpMethods.POST, uri = Uri.Path("/some/foo").toString(), headers = List(Host("localhost", 1010)), HttpEntity("some text"), HttpProtocols.`HTTP/1.1`)
+
+    def aGetHttpRequest = aPostHttpRequest.copy(method = HttpMethods.GET)
+
+    def aGetHttpRequestMatcher = HttpRequestMatcher(method = HttpMethods.GET)
+
+    def aPostHttpRequestMatcher = HttpRequestMatcher(method = HttpMethods.POST)
   }
 
 }
